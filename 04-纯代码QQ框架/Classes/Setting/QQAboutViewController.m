bf21a68fd4216909ae245bbb88e2cf2051b93919
xiaowang <xiaowang@163.com>
2017-02-05 16:10:58 +0800

    创建关于控制器

---
+++ 04-纯代码QQ框架/Classes/Setting/QQAboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  QQAboutViewController.m
+//  04-纯代码QQ框架
+//
+//  Created by HM09 on 17/2/5.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import "QQAboutViewController.h"
+
+@interface QQAboutViewController ()
+
+@end
+
+@implementation QQAboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    [self loadDataWithPlistName:@"functionAbout.plist"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
