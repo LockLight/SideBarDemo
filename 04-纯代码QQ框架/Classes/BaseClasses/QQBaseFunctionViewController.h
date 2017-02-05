926b49ef1ba508e0a2642e5da8d356cbbb9e42ec
xiaowang <xiaowang@163.com>
2017-02-05 15:46:26 +0800

    抽取基类，让功能表格界面的代码更加简洁

---
+++ 04-纯代码QQ框架/Classes/BaseClasses/QQBaseFunctionViewController.h
@@ -0,0 +1,15 @@
+//
+//  QQBaseFunctionViewController.h
+//  04-纯代码QQ框架
+//
+//  Created by HM09 on 17/2/5.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QQBaseFunctionViewController : UIViewController
+
+-(void)loadDataWithPlistName:(NSString *)plistName;
+
+@end
--- 04-纯代码QQ框架/Classes/Contact/QQAddContactViewController.h
+++ 04-纯代码QQ框架/Classes/Contact/QQAddContactViewController.h
@@ -6,8 +6,8 @@
 //  Copyright © 2017年 itheima. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "QQBaseFunctionViewController.h"
 
-@interface QQAddContactViewController : UIViewController
+@interface QQAddContactViewController : QQBaseFunctionViewController
 
 @end
